387769aa4ab4cfde3ccb5c24ff32d076bc4c245e
Tomek Strąk <entrop.tomek@gmail.com>
2018-12-11 13:45:03 +0100

    added feet to cm

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
+
 const derp = document.getElementsByClassName("postMessage");
 for (let o of derp){
 
     let text = o.innerHTML;
     const regex = new RegExp("[+-]?\\d+(?:\\.\\d+)?\\s*(lbs|lb)",'g');
+    const feet = new RegExp("\\d+\\'(\\s?(\\d+\x22?))?");
+
     text = text.replace(regex, function(y){
         const oldY = y;
         const reg2 = new RegExp("[+-]?\\d+(?:\\.\\d+)?",'g');
 
         y = y.replace(reg2, function (nb) {
             nb = nb * 0.45;
-            return nb;
+            return Math.floor(nb);
         });
         y = y.replace(/(lbs|lb)/g,'kg');
         return '<span title="'+oldY+'" style="text-decoration: underline">'+y+'</span>';
+    });
+
+    text = text.replace(feet,function (ft) {
+
+        let
+        a = ft.replace( /(\x22)/g, ''),
+        b = a.replace( /'/g, '* 12 +'),
+        c = eval(b);
+        c *= 2.54;
+
+        return '<span title="'+ft+'" style="text-decoration: underline">'+Math.floor(c)+'cm'+'</span>';
     });
 
     o.innerHTML = text;
